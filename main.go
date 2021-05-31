7731c483691546cc5ea000cf2729953f6166289b
Xiangxu Liu <xxliu@toughtworks.com>
2021-05-31 17:00:59 +0800

    add product

--- main.go
+++ main.go
@@ -17,12 +17,12 @@ var bindAddress = env.String("BIND_ADDRESS", false, ":9090", "Bind address for t
 func main() {
 	env.Parse()
 
-	l := log.New(os.Stdout, "[product-api]", log.LstdFlags)
-	hh := handlers.NewHello(l)
-
+	l := log.New(os.Stdout, "[product-api] ", log.LstdFlags)
+	// create the handlers
+	ph := handlers.NewProducts(l)
 	// create a new serve mux and register the handlers
 	sm := http.NewServeMux()
-	sm.Handle("/", hh)
+	sm.Handle("/", ph)
 
 	// create a new server
 	s := http.Server{
